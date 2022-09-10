8676656dc522b40d12ab628f8a9e8d44d026bd56
Chulhy <ekxk1234@naver.com>
2022-09-10 17:31:06 +0900

    first commit

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,44 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10%할인이 적용되어야 한다.")
+    void vip_o(){
+        //given
+        Member member = new Member(1L,"second member", Grade.VIP);
+
+        //when
+        int discount1 = discountPolicy.discount(member, 20000);
+
+        //then
+        assertThat(discount1).isEqualTo(2000);
+
+    }
+
+    @Test
+    @DisplayName("VIP가 아닌경우")
+    void vip_x(){
+        //given
+        Member member = new Member(1L,"second member", Grade.BASIC);
+
+        //when
+        int discount1 = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount1).isEqualTo(1000);
+    }
+
+
+}
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,31 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+
+    @Test
+    void join(){
+        //given , ~주어졌을 때
+        Member member = new Member(1L,"member A" , Grade.VIP);
+
+        //when , ~이렇게 했을 때
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then , ~결과
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
